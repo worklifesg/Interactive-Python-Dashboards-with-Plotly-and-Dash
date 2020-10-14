c11de5402bfc720f1a9736f333a2bd35c3de4a02
Shraman Gupta <worklife_sg@outlook.com>
2020-10-14 10:04:04 -0400

    Create 1.Dash_Basic.py

---
+++ Dash/1.Dash_Basic.py
@@ -0,0 +1,19 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html 
+import plotly.offline as pyo
+
+#create application
+
+app = dash.Dash() #creating like a Flask application
+
+#Layouts
+#Create a division in dashboard to innert links
+#with variable children that has a list
+app.layout = html.Div(children=[
+            html.H1('Hello Dash!') #header
+]) 
+
+if __name__ == '__main__':
+    app.run_server() # just checks if we running our script or not, grab the application ('app') and run the server
+
